7bcc41c7583006004b733f1a35566d4f2708e0bd
Sunny Pereira <sampleemail@gmail.com>
2020-05-08 23:13:36 -0400

    initial commit

---
+++ src/test/java/org/lucas/sunny/quarkus/micro/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.lucas.sunny.quarkus.micro;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
